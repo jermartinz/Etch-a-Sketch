5870d957382a03ddc24937d5a14a92db80f43034
jermartinz <61643086+jermartinz@users.noreply.github.com>
2022-07-06 21:31:16 -0400

    Add fuction to make square divs

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const mainContainer = document.querySelector('#main-container')
+
+function genDiv(rows, cols){
+    mainContainer.style.setProperty('--grid-rows', rows);
+    mainContainer.style.setProperty('--grid-cols', cols);
+    
+    for(c = 0; c < (rows * cols); c++){
+        let cell = document.createElement('div');
+        mainContainer.appendChild(cell).className = 'grid-item';
+    }
+    
+}
+genDiv(60, 120)
